b77cd79b268d044eaa6ed80ecc6c1b44d3d703e9
davidtan <davidtan@thoughtworks.com>
2018-02-13 14:30:26 +0800

    Update seed data

--- src/profiles.js
+++ src/profiles.js
@@ -5,30 +5,218 @@ export default [
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae tortor vel lectus interdum suscipit. Aliquam erat volutpat. Donec ac lorem nibh. Fusce et suscipit turpis. Fusce et ultricies mauris. Sed sodales diam pharetra, suscipit enim id, ullamcorper lacus. Sed nunc eros, tristique sit amet dui non, fermentum placerat erat. Aliquam eu tortor ex. Ut fermentum nec sem id blandit. Nullam in interdum dolor. Integer aliquet ultrices nisi sed sollicitudin.",
     tagline: "i am a cat",
-    skills: ["ux", "development", "tdd", "agile"]
+    skills: ["ux", "development", "tdd", "agile"],
+    contact_number: 91234568,
+    email_address: "email@email.com",
+    hard_competencies: [
+      { javascript: 9 },
+      { react: 10 },
+      { TDD: 7 },
+      { agile: 5 },
+      { "domain-driven design": 8 }
+    ],
+    soft_competencies: [
+      { communication: 9 },
+      { agile: 10 },
+      { teamwork: 7 },
+      { presentation: 5 }
+    ],
+    job_experience: [
+      {
+        job_role: "dev",
+        employer: "dunkin donuts",
+        start_date: "01-01-2015",
+        end_date: "01-01-2016",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae tortor vel lectus interdum suscipit. Aliquam erat volutpat. Donec ac lorem nibh. Fusce et suscipit turpis. Fusce et ultricies mauris. Sed sodales diam pharetra, suscipit enim id, ullamcorper lacus. Sed nunc eros, tristique sit amet dui non, fermentum placerat erat. Aliquam eu tortor ex. Ut fermentum nec sem id blandit. Nullam in interdum dolor. Integer aliquet ultrices nisi sed sollicitudin."
+      },
+      {
+        job_role: "dev",
+        employer: "dunkin donuts",
+        start_date: "01-01-2015",
+        end_date: "01-01-2016",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae tortor vel lectus interdum suscipit. Aliquam erat volutpat. Donec ac lorem nibh. Fusce et suscipit turpis. Fusce et ultricies mauris. Sed sodales diam pharetra, suscipit enim id, ullamcorper lacus. Sed nunc eros, tristique sit amet dui non, fermentum placerat erat. Aliquam eu tortor ex. Ut fermentum nec sem id blandit. Nullam in interdum dolor. Integer aliquet ultrices nisi sed sollicitudin."
+      }
+    ],
+    projects: [
+      {
+        name: "my awesome project",
+        description: "an application to serve cute cats",
+        image_url: "http://thecatapi.com/api/images/get?format=src",
+        tech_stack: ["react", "rails", "express", "travisCI"]
+      },
+      {
+        name: "my awesome project",
+        description: "an application to serve cute cats",
+        image_url: "http://thecatapi.com/api/images/get?format=src",
+        tech_stack: ["react", "rails", "express", "travisCI"]
+      }
+    ]
   },
   {
     id: "2",
     name: "alice",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae tortor vel lectus interdum suscipit. Aliquam erat volutpat. Donec ac lorem nibh. Fusce et suscipit turpis. Fusce et ultricies mauris. Sed sodales diam pharetra, suscipit enim id, ullamcorper lacus. Sed nunc eros, tristique sit amet dui non, fermentum placerat erat. Aliquam eu tortor ex. Ut fermentum nec sem id blandit. Nullam in interdum dolor. Integer aliquet ultrices nisi sed sollicitudin.",
-    tagline: "i love programming",
-    skills: ["ux", "development", "tdd", "agile"]
+    tagline: "i am a cat",
+    skills: ["ux", "development", "tdd", "agile"],
+    contact_number: 91234568,
+    email_address: "email@email.com",
+    hard_competencies: [
+      { javascript: 9 },
+      { react: 10 },
+      { TDD: 7 },
+      { agile: 5 },
+      { "domain-driven design": 8 }
+    ],
+    soft_competencies: [
+      { communication: 9 },
+      { agile: 10 },
+      { teamwork: 7 },
+      { presentation: 5 }
+    ],
+    job_experience: [
+      {
+        job_role: "dev",
+        employer: "dunkin donuts",
+        start_date: "01-01-2015",
+        end_date: "01-01-2016",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae tortor vel lectus interdum suscipit. Aliquam erat volutpat. Donec ac lorem nibh. Fusce et suscipit turpis. Fusce et ultricies mauris. Sed sodales diam pharetra, suscipit enim id, ullamcorper lacus. Sed nunc eros, tristique sit amet dui non, fermentum placerat erat. Aliquam eu tortor ex. Ut fermentum nec sem id blandit. Nullam in interdum dolor. Integer aliquet ultrices nisi sed sollicitudin."
+      },
+      {
+        job_role: "dev",
+        employer: "dunkin donuts",
+        start_date: "01-01-2015",
+        end_date: "01-01-2016",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae tortor vel lectus interdum suscipit. Aliquam erat volutpat. Donec ac lorem nibh. Fusce et suscipit turpis. Fusce et ultricies mauris. Sed sodales diam pharetra, suscipit enim id, ullamcorper lacus. Sed nunc eros, tristique sit amet dui non, fermentum placerat erat. Aliquam eu tortor ex. Ut fermentum nec sem id blandit. Nullam in interdum dolor. Integer aliquet ultrices nisi sed sollicitudin."
+      }
+    ],
+    projects: [
+      {
+        name: "my awesome project",
+        description: "an application to serve cute cats",
+        image_url: "http://thecatapi.com/api/images/get?format=src",
+        tech_stack: ["react", "rails", "express", "travisCI"]
+      },
+      {
+        name: "my awesome project",
+        description: "an application to serve cute cats",
+        image_url: "http://thecatapi.com/api/images/get?format=src",
+        tech_stack: ["react", "rails", "express", "travisCI"]
+      }
+    ]
   },
   {
     id: "3",
-    name: "tim",
+    name: "jane",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae tortor vel lectus interdum suscipit. Aliquam erat volutpat. Donec ac lorem nibh. Fusce et suscipit turpis. Fusce et ultricies mauris. Sed sodales diam pharetra, suscipit enim id, ullamcorper lacus. Sed nunc eros, tristique sit amet dui non, fermentum placerat erat. Aliquam eu tortor ex. Ut fermentum nec sem id blandit. Nullam in interdum dolor. Integer aliquet ultrices nisi sed sollicitudin.",
-    tagline: "i love cats and i love programming",
-    skills: ["ux", "development", "tdd", "agile"]
+    tagline: "i am a cat",
+    skills: ["ux", "development", "tdd", "agile"],
+    contact_number: 91234568,
+    email_address: "email@email.com",
+    hard_competencies: [
+      { javascript: 9 },
+      { react: 10 },
+      { TDD: 7 },
+      { agile: 5 },
+      { "domain-driven design": 8 }
+    ],
+    soft_competencies: [
+      { communication: 9 },
+      { agile: 10 },
+      { teamwork: 7 },
+      { presentation: 5 }
+    ],
+    job_experience: [
+      {
+        job_role: "dev",
+        employer: "dunkin donuts",
+        start_date: "01-01-2015",
+        end_date: "01-01-2016",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae tortor vel lectus interdum suscipit. Aliquam erat volutpat. Donec ac lorem nibh. Fusce et suscipit turpis. Fusce et ultricies mauris. Sed sodales diam pharetra, suscipit enim id, ullamcorper lacus. Sed nunc eros, tristique sit amet dui non, fermentum placerat erat. Aliquam eu tortor ex. Ut fermentum nec sem id blandit. Nullam in interdum dolor. Integer aliquet ultrices nisi sed sollicitudin."
+      },
+      {
+        job_role: "dev",
+        employer: "dunkin donuts",
+        start_date: "01-01-2015",
+        end_date: "01-01-2016",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae tortor vel lectus interdum suscipit. Aliquam erat volutpat. Donec ac lorem nibh. Fusce et suscipit turpis. Fusce et ultricies mauris. Sed sodales diam pharetra, suscipit enim id, ullamcorper lacus. Sed nunc eros, tristique sit amet dui non, fermentum placerat erat. Aliquam eu tortor ex. Ut fermentum nec sem id blandit. Nullam in interdum dolor. Integer aliquet ultrices nisi sed sollicitudin."
+      }
+    ],
+    projects: [
+      {
+        name: "my awesome project",
+        description: "an application to serve cute cats",
+        image_url: "http://thecatapi.com/api/images/get?format=src",
+        tech_stack: ["react", "rails", "express", "travisCI"]
+      },
+      {
+        name: "my awesome project",
+        description: "an application to serve cute cats",
+        image_url: "http://thecatapi.com/api/images/get?format=src",
+        tech_stack: ["react", "rails", "express", "travisCI"]
+      }
+    ]
   },
   {
     id: "4",
-    name: "tom",
+    name: "tim",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae tortor vel lectus interdum suscipit. Aliquam erat volutpat. Donec ac lorem nibh. Fusce et suscipit turpis. Fusce et ultricies mauris. Sed sodales diam pharetra, suscipit enim id, ullamcorper lacus. Sed nunc eros, tristique sit amet dui non, fermentum placerat erat. Aliquam eu tortor ex. Ut fermentum nec sem id blandit. Nullam in interdum dolor. Integer aliquet ultrices nisi sed sollicitudin.",
-    tagline: "i am the 44th president of the united states",
-    skills: ["ux", "development", "tdd", "agile"]
+    tagline: "i am a cat",
+    skills: ["ux", "development", "tdd", "agile"],
+    contact_number: 91234568,
+    email_address: "email@email.com",
+    hard_competencies: [
+      { javascript: 9 },
+      { react: 10 },
+      { TDD: 7 },
+      { agile: 5 },
+      { "domain-driven design": 8 }
+    ],
+    soft_competencies: [
+      { communication: 9 },
+      { agile: 10 },
+      { teamwork: 7 },
+      { presentation: 5 }
+    ],
+    job_experience: [
+      {
+        job_role: "dev",
+        employer: "dunkin donuts",
+        start_date: "01-01-2015",
+        end_date: "01-01-2016",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae tortor vel lectus interdum suscipit. Aliquam erat volutpat. Donec ac lorem nibh. Fusce et suscipit turpis. Fusce et ultricies mauris. Sed sodales diam pharetra, suscipit enim id, ullamcorper lacus. Sed nunc eros, tristique sit amet dui non, fermentum placerat erat. Aliquam eu tortor ex. Ut fermentum nec sem id blandit. Nullam in interdum dolor. Integer aliquet ultrices nisi sed sollicitudin."
+      },
+      {
+        job_role: "dev",
+        employer: "dunkin donuts",
+        start_date: "01-01-2015",
+        end_date: "01-01-2016",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae tortor vel lectus interdum suscipit. Aliquam erat volutpat. Donec ac lorem nibh. Fusce et suscipit turpis. Fusce et ultricies mauris. Sed sodales diam pharetra, suscipit enim id, ullamcorper lacus. Sed nunc eros, tristique sit amet dui non, fermentum placerat erat. Aliquam eu tortor ex. Ut fermentum nec sem id blandit. Nullam in interdum dolor. Integer aliquet ultrices nisi sed sollicitudin."
+      }
+    ],
+    projects: [
+      {
+        name: "my awesome project",
+        description: "an application to serve cute cats",
+        image_url: "http://thecatapi.com/api/images/get?format=src",
+        tech_stack: ["react", "rails", "express", "travisCI"]
+      },
+      {
+        name: "my awesome project",
+        description: "an application to serve cute cats",
+        image_url: "http://thecatapi.com/api/images/get?format=src",
+        tech_stack: ["react", "rails", "express", "travisCI"]
+      }
+    ]
   }
 ];
